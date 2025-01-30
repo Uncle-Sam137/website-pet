2d5f6c94d4af16b7cda520b2ac6db5295f895907
Uncle-Sam137 <semen.kolesnik@yandex.ru>
2025-01-30 20:36:41 +0300

    Добавил в представление работу с формой

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import JobOfferForm
+from .models import JobOffer
 
 def home(request):
-    return render(request, 'base.html')
+    if request.method == 'POST':
+        form = JobOfferForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+
+            return HttpResponseRedirect('/')
+
+    else:
+        form = JobOfferForm()
+
+    return render(request, 'base.html', {'form': form})
+
 
 
